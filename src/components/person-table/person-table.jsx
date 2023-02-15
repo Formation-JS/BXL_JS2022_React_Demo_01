0dbb6250f5411f4c6a38b37335d7344d8c653b0d
Aurélien <aurelien.strimelle@bstorm.be>
2023-02-15 12:02:57 +0100

    Demo 02 - Collection

---
+++ src/components/person-table/person-table.jsx
@@ -0,0 +1,41 @@
+const PersonTableHead = () => (
+    <thead>
+        <tr>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+);
+
+const PersonTableRow = ({ firstname, lastname, age }) => (
+    <tr>
+        <td>{firstname}</td>
+        <td>{lastname}</td>
+        <td>{age}</td>
+    </tr>
+);
+
+
+const PersonTable = ({ data }) => {
+
+    // Transfomation des données en format JSX
+    const dataJSX = data.map(
+        person => <PersonTableRow {...person} key={person.id} />
+    );
+
+    return (
+        <table>
+            <PersonTableHead />
+            <tbody>
+                {dataJSX}
+            </tbody>
+        </table>
+    );
+};
+
+PersonTable.defaultProps = {
+    data: []
+};
+
+export default PersonTable;
